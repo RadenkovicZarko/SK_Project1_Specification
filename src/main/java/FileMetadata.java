99951035484f03252fe445495c4197c46a7b58e3
Zarko Radenkovic <zradenkovic520rn@raf.rs>
2022-10-27 23:04:45 +0200

    FileMetadata atributes

--- src/main/java/FileMetadata.java
+++ src/main/java/FileMetadata.java
@@ -6,9 +6,18 @@ import lombok.Setter;
 
 public class FileMetadata {
   private int size;
-
+  private String date;
+  private String type;
+  private String extensions;
+  private String name;
   //TODO - add more metadata
-  public FileMetadata(int size) {
+
+
+  public FileMetadata(int size, String date, String type, String extensions, String name) {
     this.size = size;
+    this.date = date;
+    this.type = type;
+    this.extensions = extensions;
+    this.name = name;
   }
 }
