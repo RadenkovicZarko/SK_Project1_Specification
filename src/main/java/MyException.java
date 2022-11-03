ace63ce870a94471b349b8d9d2af42cfb302017b
cvele0 <vladancvjetkovic@gmail.com>
2022-11-04 00:22:26 +0100

    change

--- src/main/java/MyException.java
+++ src/main/java/MyException.java
@@ -1,7 +1,13 @@
 public class MyException extends RuntimeException {
   private String message;
+  private boolean successful;
 
   public MyException(String message) {
     this.message = message;
+  }
+
+  public MyException(String message, boolean successful) {
+    this.message = message;
+    this.successful = successful;
   }
 }
