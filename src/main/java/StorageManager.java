1e6cae72852b564dd2834aaedbec4c0c7568a68d
Zarko Radenkovic <zradenkovic520rn@raf.rs>
2022-11-26 00:48:57 +0100

    Commit

--- src/main/java/StorageManager.java
+++ src/main/java/StorageManager.java
@@ -4,14 +4,7 @@ public class StorageManager {
   public static void registerStorage(StorageSpecification st) {
     storage = st;
   }
-
-//  public static StorageSpecification getStorage(String path) {
-//    if (StorageManager.storage != null) {
-//      StorageManager.storage.setRootFolderPath(path);
-//    }
-//    return StorageManager.storage;
-//  }
-
+  
   public static StorageSpecification getStorage() {
     return StorageManager.storage;
   }
